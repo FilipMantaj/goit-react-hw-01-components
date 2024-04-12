dc0bdec84e11e9f35dace099969c6ffba9bb6230
FilipMantaj <filip.mantaj17@o2.pl>
2024-04-12 23:15:45 +0200

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import items from '../data/transactions.json';
+
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+// import { FriendListItem } from './FriendListItem';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile user={user}></Profile>
+      <Statistics data={data}></Statistics>
+      <FriendList friends={friends}></FriendList>
+      <TransactionHistory items={items}></TransactionHistory>
     </div>
   );
 };
